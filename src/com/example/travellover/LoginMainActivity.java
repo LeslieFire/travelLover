33a37a5f6bb5afa47b8c268a5805b551eda466c4
Yang Fei <249893977@qq.com>
2014-10-30 16:53:12 +0800

    添加地图定位功能
    在login_main中添加两个按钮，分别对应地图定位，路线规划功能

--- src/com/example/travellover/LoginMainActivity.java
+++ src/com/example/travellover/LoginMainActivity.java
@@ -1,5 +1,7 @@
 package com.example.travellover;
 
+import com.travellover.map.MapLocationActivity;
+import com.travellover.map.MapRoutePlanActivity;
 import com.travellover.register.RegisterUserFirstActivity;
 import com.travellovercar.register.LoginCarActivity;
 import com.travellovercar.register.RegisterCarFirstActivity;
@@ -42,6 +44,35 @@ public class LoginMainActivity extends Activity {
 			}
 			
 		});
+		
+		// 添加地图测试相关按钮  add by leslie 2014/10/30  begin
+		
+		Button locatBtn = (Button)findViewById(R.id.bMapLocat);
+		locatBtn.setOnClickListener(new Button.OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent();
+				intent.setClass(LoginMainActivity.this, MapLocationActivity.class);
+				startActivity(intent);
+			}
+			
+		});
+		
+		Button RoutePlanBtn = (Button)findViewById(R.id.bRoutePlan);
+		RoutePlanBtn.setOnClickListener(new Button.OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent();
+				intent.setClass(LoginMainActivity.this, MapRoutePlanActivity.class);
+				startActivity(intent);
+			}
+			
+		});
+		// 添加地图定位按钮  add by leslie 2014/10/30  end
 	}
 
 }
